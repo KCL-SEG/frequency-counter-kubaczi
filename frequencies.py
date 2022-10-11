29c9ce48815888d7009ae0361f1f67faeb41771d
Jakub Czerwik <jakub.czerwik@kcl.ac.uk>
2022-10-11 14:58:52 +0100

    Add frequency counting functionality and non-string to string conversion of inputs

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,13 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for value in items:
+        if not isinstance(value,str):
+            value=str(value)
+        if not value in frequencies:
+            frequencies[f'{value}']=1
+        else:
+            frequencies[f'{value}']=frequencies[f'{value}']+1
     return frequencies
